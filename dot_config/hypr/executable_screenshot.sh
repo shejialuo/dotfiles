057f82a78d6674f3b96abeb71148e56b3ec5ff59
shejialuo <shejialuo@gmail.com>
2026-03-06 21:50:18 +0800

    luo-arch: update hypr dunst and mpd

--- dot_config/hypr/executable_screenshot.sh
+++ dot_config/hypr/executable_screenshot.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-grim -g "$(slurp -c '#ff0000ff')" -t ppm - | satty --filename - --fullscreen --output-filename ~/Pictures/Screenshots/satty-$(date '+%Y%m%d-%H:%M:%S').png
+grim -g "$(slurp -c '#00000000')" - | satty --filename - --fullscreen --output-filename ~/Pictures/Screenshots/satty-$(date '+%Y%m%d-%H:%M:%S').png
